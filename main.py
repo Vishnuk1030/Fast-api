1cb583bc82f3c90c656d0344eb2511a299aa856a
vishnuavanexa <vishnuavanexa@gmail.com>
2025-01-13 19:53:53 +0530

    todo api added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from typing import Optional
+from fastapi import FastAPI,HTTPException
+from typing import Optional,List
 
 from pydantic import BaseModel
 
@@ -15,7 +15,13 @@ class PackageIn(BaseModel):
     number: int
     description: Optional[str]=None
     
+class Todo(BaseModel):
+    name:str
+    due_date:str
+    description:str
+    
 app = FastAPI()
+
 
 # pydantic BaseModel
 # @app.post('/package/{priority}')
@@ -50,3 +56,43 @@ async def getComponet(component_id:int):
 async def read_components(number:int,text:str):
     return {"number":number,"text":text}
 
+#create,Read,Update and Delete, Todo API
+
+store_todo=[]
+
+@app.get('/getTodo')
+async def todo_get():
+    return {"Hello":"world"}
+
+@app.post('/PostTodo')
+async def todo_post(todo:Todo):
+    store_todo.append(todo)
+    return todo
+
+@app.get('/todo/',response_model=List[Todo])
+async def todo_getall():
+    return store_todo
+
+@app.get('/todo/{id}')
+async def get_one_todo(id:int):
+    try:
+        return store_todo[id]
+    except:
+        raise HTTPException(status_code=404,detail="Todo not found")
+    
+@app.put('/todo/{id}')
+async def update_todo(id:int,todo:Todo):
+    try:
+        store_todo[id]=todo
+        return store_todo[id]
+    except:
+        raise HTTPException(status_code=404,detail="Todo not found")
+
+@app.delete('/todo/{id}')
+async def delete_todo(id:int):
+    try:
+        obj=store_todo[id]
+        store_todo.pop(id)
+        return obj
+    except:
+        raise HTTPException(status_code=404,detail="Todo not found")
