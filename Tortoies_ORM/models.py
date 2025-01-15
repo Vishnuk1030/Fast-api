9aa35ce9ca677a40a95280447885ff6f2b44e802
vishnuavanexa <vishnuavanexa@gmail.com>
2025-01-15 10:45:19 +0530

    Tortoise ORM with FastAPI Integration

--- Tortoies_ORM/models.py
+++ Tortoies_ORM/models.py
@@ -3,6 +3,7 @@ from tortoise import fields,models
 from tortoise.contrib.pydantic import pydantic_model_creator
 
 class Todo(models.Model):
+    
     id=fields.IntField(pk=True)
     todo=fields.CharField(max_length=250)
     due_date=fields.CharField(max_length=250)
